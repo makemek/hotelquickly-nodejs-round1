4876fae4e553ce2c0be626cc8d5ccc2821c48206
Makemek <makemek101@hotmail.com>
2016-08-04 17:48:25 +0700

    parameterize test method for paypal and braintree

--- test/test-payment.js
+++ test/test-payment.js
@@ -3,30 +3,31 @@
 const paypal = require('../src/service/paypal');
 const braintree = require('../src/service/braintree');
 
-describe('payment', function() {
+[{name: 'paypal', component: paypal}, {name: 'braintree', component: braintree}]
+.forEach(function(item){
+	describe(item.name, function(){
+		var creditCard;
 
-	var creditCard;
+		beforeEach(function(){
+			creditCard = {
+				pay_method: 'visa',
+				card_number: '4111111111111111',
+				card_expire_month: '9',
+				card_expire_year: '2021',
+				card_cvv: '874',
+				card_holder_firstname: 'Joe',
+				card_holder_lastname: 'Shopper'
+			}
+		})
 
-	beforeEach(function() {
-		creditCard = {
-			pay_method: 'visa',
-			card_number: '4111111111111111',
-			card_expire_month: '9',
-			card_expire_year: '2021',
-			card_cvv: '874',
-			card_holder_firstname: 'Joe',
-			card_holder_lastname: 'Shopper',
-		}
-	})
-
-	describe('paypal', function() {
 		it('valid credit card number', function(done) {
 			this.timeout(50000);
-			paypal.charge(creditCard, 1, 'usd', function(error, isSuccess, result) {
+			item.component.charge(creditCard, 1, 'usd', function(error, isSuccess, result) {
 				if(error)
 					return done(error);
 
 				assert.isTrue(isSuccess);
+				assert.isDefined(result);
 				done();
 			})
 		})
@@ -34,36 +35,12 @@ describe('payment', function() {
 		it('invalid credit card number', function(done){
 			this.timeout(50000);
 			creditCard.card_number = '0123456789';
-			paypal.charge(creditCard, 1, 'usd', function(error, isSuccess, result) {
+			item.component.charge(creditCard, 1, 'usd', function(error, isSuccess, result) {
 				if(error)
 					return done(error);
 
 				assert.isFalse(isSuccess);
-				done();
-			})
-		})
-	})
-
-	describe('braintree', function() {
-		it('valid credit card number', function(done) {
-			this.timeout(50000);
-			braintree.charge(creditCard, 1, 'usd', function(error, isSuccess, result) {
-				if(error)
-					return done(error);
-				
-				assert.isTrue(isSuccess, result.message);
-				done();
-			})
-		})
-
-		it('invalid credit card number', function(done){
-			this.timeout(50000);
-			creditCard.card_number = '0123456789';
-			paypal.charge(creditCard, 1, 'usd', function(error, isSuccess, result) {
-				if(error)
-					return done(error);
-
-				assert.isFalse(isSuccess);
+				assert.isDefined(result);
 				done();
 			})
 		})
