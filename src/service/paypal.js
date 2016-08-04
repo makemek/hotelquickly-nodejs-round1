fa8063491b4d0d4381fed08cef5e74d01964404c
Makemek <makemek101@hotmail.com>
2016-08-04 14:33:57 +0700

    add documentation for Paypal.charge() method

--- src/service/paypal.js
+++ src/service/paypal.js
@@ -6,6 +6,24 @@ const paymentConfig = require('../config/payment');
 paypal.configure(paymentConfig.paypal);
 
 var Paypal = {
+	/**
+	 * Response from paypal sdk
+	 * Example {@link https://github.com/paypal/PayPal-node-SDK/blob/master/samples/payment/create_with_credit_card.js}
+	 * 
+	 * @callback {requestCallback}
+	 * @param {object} error
+	 * @param {object} result - response from paypal sdk. Sample {@link https://developer.paypal.com/docs/api/payments/#payment_execute_response}
+	 */
+
+	/**
+	 * Charge payer's money through credit card
+	 * All validation tasks are delegated to paypal
+	 * 
+	 * @param  {object}   creditCard - credit card information
+	 * @param  {number}   amount     - total amount to charge
+	 * @param  {string}   currency   - an ISO 4217 currency code {@link https://en.wikipedia.org/wiki/ISO_4217}
+	 * @param  {requestCallback} done - a callback function that handles the response
+	 */
 	charge: function(creditCard, amount, currency, done) {
 		var card = mapCreditcardToPaypalFormat(creditCard);
 		var order = createTransaction(amount, currency);
