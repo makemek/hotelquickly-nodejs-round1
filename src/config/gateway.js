774f3f7863f2d80970558989548fb1fef9a4e640
Makemek <makemek101@hotmail.com>
2016-08-05 01:56:38 +0700

    configure gateway routing table

---
+++ src/config/gateway.js
@@ -0,0 +1,28 @@
+'use strcit';
+
+const paypal = require('../service/paypal');
+const braintree = require('../service/braintree');
+
+/**
+ * Gateway payment service routing table
+ * @type {Array} contains a mapping between source's card information and target payment service
+ *
+ * <source> consist of
+ *  - cardType {string} type of credit card (case-insensitive)
+ *  - currency {string[]} array of ISO 4217 currency unit (case-insensitive)
+ *    If source's currency value found in the array, it is considered valid
+ * You can use '*' to mean any kind of type
+ * 
+ * <target> is the detinated payment service
+ *
+ * Please note that higher index in the array means higher priority
+ * Meaning that if there are duplicates, the upper-most one is selected
+ */
+const GATEWAY = [
+{source: {cardType: 'AMEX', currency: ['USD']}, target: paypal},
+{source: {cardType: 'AMEX', currency: '*'}, target: new Error('AMEX can be use with USD only')},
+{source: {cardType: '*', currency: ['USD', 'EUR', 'AUD']}, target: paypal},
+{source: {cardType: '*', currency: '*'}, target: braintree}
+]
+
+module.exports = exports = GATEWAY;
