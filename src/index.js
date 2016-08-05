7636cf4bb06e692ee97bb8243f5138061438587d
Makemek <makemek101@hotmail.com>
2016-08-05 20:28:04 +0700

    setup express to retrive POST parameters

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const express = require('express');
+const bodyparser = require('body-parser');
 const app = express();
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static('src/public'));
 app.use('/', require('./route/'));
 
